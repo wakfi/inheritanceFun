c9105ea58d30b208db14b2e9439fe27c1b447781
Walker Gray <55608093+wakfi@users.noreply.github.com>
2020-09-18 11:03:10 -0700

    Initial Commit

---
+++ inheritanceFun/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  inheritanceFun
+//
+//  Created by Gray, John Walker on 9/18/20.
+//  Copyright © 2020 Walker Gray. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
